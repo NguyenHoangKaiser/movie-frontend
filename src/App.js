c6626dced80268b7549a089033bfbef0af54f788
HuyHoang <dragonhuyhoang2001@gmail.com>
2022-10-06 13:03:53 +0700

    Define Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Switch, Route, Link} from "react-router-dom";
+import { Route, Link, Routes } from "react-router-dom";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import AddReview from "./components/add-review";
+import MoviesList from "./components/movies-list";
+import Movie from "./components/movie";
+import Login from "./components/login";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  };
+
+  async function logout() {
+    setUser(null);
+  };
+
   return (
     <div className="App">
-      Hello
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            {/* <Nav.Link as={Link} to={"/add-review"}>
+              Add Review
+            </Nav.Link> */}
+            <Nav.Link>
+              {user ? <a onClick={logout}>Logout User</a> : <Link to={"/login"}>Login</Link>}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      <Routes>
+        <Route path="/movies" element={<MoviesList />} />
+        <Route path="/movies/:id" element={<Movie />} />
+        {/* <Route path="/add-review" element={<AddReview />} /> */}
+        <Route path="/login" element={<Login login={login} />} />
+      </Routes>
+
     </div>
   );
 }
